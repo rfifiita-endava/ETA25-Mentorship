b292813876a734880e3c7fae4f145579aab17a25
ENDAVA\RFifiita <raduconstantin.fifiita@endava.com>
2024-12-03 14:44:23 +0200

    Session 5 code

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleAppETA25;
+using System;
 //Console.WriteLine("Hello, World!");
 
 /*int number = 10;
@@ -25,32 +26,92 @@ Console.Write(c);
 Console.Write(str);
 Console.Write(guid);*/
 
-Console.WriteLine("Please input your First name:");
-string firstName = Console.ReadLine();
+//Console.WriteLine("Please input your First name:");
+//string firstName = Console.ReadLine();
 
-Console.WriteLine("Please input your Last name:");
-string lastName = Console.ReadLine();
+//Console.WriteLine("Please input your Last name:");
+//string lastName = Console.ReadLine();
 
-Console.WriteLine("Please input your age:");
-int age = Convert.ToInt16(Console.ReadLine());
-var computedAge = age + 20;
+//Console.WriteLine("Please input your age:");
+//int age = Convert.ToInt16(Console.ReadLine());
+//var computedAge = age + 20;
 
-Console.WriteLine("Please input your gender:");
-string gender = Console.ReadLine();
+//Console.WriteLine("Please input your gender:");
+//string gender = Console.ReadLine();
 
-Console.WriteLine($"\n Your details are as follows:\n" +
-    $"  - First name: {firstName} \n" +
-    $"  - Last name: {lastName} \n" +
-    $"  - Gender: {gender} \n" +
-    $"You will be {computedAge} year old in 20 years!");
+//Console.WriteLine($"\n Your details are as follows:\n" +
+//    $"  - First name: {firstName} \n" +
+//    $"  - Last name: {lastName} \n" +
+//    $"  - Gender: {gender} \n" +
+//    $"You will be {computedAge} year old in 20 years!");
 
-Console.WriteLine();
-Console.WriteLine();
-Console.WriteLine($"Your details are as follows:");
-Console.WriteLine($"  - First name: {firstName}");
-Console.WriteLine($"  - Last name: {lastName}");
-Console.WriteLine($"  - Gender: {gender}");
-Console.WriteLine($"You will be {computedAge} year old in 20 years!");
+//Console.WriteLine();
+//Console.WriteLine();
+//Console.WriteLine($"Your details are as follows:");
+//Console.WriteLine($"  - First name: {firstName}");
+//Console.WriteLine($"  - Last name: {lastName}");
+//Console.WriteLine($"  - Gender: {gender}");
+//Console.WriteLine($"You will be {computedAge} year old in 20 years!");
 
+
+
+// Session - C# Basics - Operators
+Console.WriteLine($"The result of ADDITION is: {1 + 1}");
+Console.WriteLine($"The result of SUBSTRACTION is: {3 - 1}");
+Console.WriteLine($"The result of MULTIPLICATION is: {3 * 3}");
+Console.WriteLine($"The result of DIVISION is: {5 / 5}");
+Console.WriteLine($"The result of MODULUS is: {7 % 5}");
+
+var x = 10;
+var y = x;
+//Console.WriteLine($"The var 'x' has a value of: {x}");
+//Console.WriteLine();
+//Console.WriteLine($"After PRE INCREMENTATION var 'x' has a value of: {++x}");
+//Console.WriteLine($"After POST INCREMENTATION var 'x' has a value of: {x++}");
+//Console.WriteLine();
+//Console.WriteLine($"After PRE DECREMENTATION var 'x' has a value of: {--x}");
+//Console.WriteLine($"After POST DECREMENTATION var 'x' has a value of: {x--}");
+//Console.WriteLine();
+//Console.WriteLine($"The var 'x' has a value of: {x}");
+
+//Console.WriteLine($"The initial value of 'y' is: {y}");         // 10
+
+//y += 5;
+//Console.WriteLine($"The result of ADDITION is: {y}");           // 15
+
+//y -= 3;
+//Console.WriteLine($"The result of SUBSTRACTION is: {y}");       // 12
+
+//y *= 2;
+//Console.WriteLine($"The result of MULTIPLICATION is: {y}");     // 24
+
+//y /= 2;
+//Console.WriteLine($"The result of DIVISION is: {y}");           // 12
+
+//y %= 5;
+//Console.WriteLine($"The result of MODULUS is: {y}");            // 2
+//Console.WriteLine();
+//Console.WriteLine();
+
+//Console.WriteLine($"The result of '==' between '{x}' and '{y}' is: {x == y}");
+//Console.WriteLine($"The result of '!=' between '{x}' and '{y}' is: {x != y}");
+//Console.WriteLine($"The result of '>' between '{x}' and '{y}' is: {x > y}");
+//Console.WriteLine($"The result of '<' between '{x}' and '{y}' is: {x < y}");
+//Console.WriteLine($"The result of '>=' between '{x}' and '{y}' is: {x >= y}");
+//Console.WriteLine($"The result of '<=' between '{x}' and '{y}' is: {x <= y}");
+
+
+bool raspuns = false;
+bool raspuns2 = true;
+
+Console.WriteLine($"The NEGATION of raspuns is: {!raspuns}");               // TRUE
+Console.WriteLine($"The && operation output is: {raspuns && raspuns2}");    // FALSE
+Console.WriteLine($"The || operation output is: {raspuns || raspuns2}");    // TRUE
+
+
+Console.WriteLine($"The value of 'x' from Session5 class is: {Session5.x}");
+Console.WriteLine($"The value of 'y' from Session5 class is: {Session5.y}");
+Session5.AddXAndY();
+Console.WriteLine($"The result of adding x and y is:"); // 24
 
 Console.ReadKey();
---
+++ Session5.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppETA25;
+
+public static class Session5
+{
+    public static int x = 5;
+    public static int y = 10;
+
+    [Test]
+    public static void AddXAndY()
+    {
+        Console.WriteLine($"The value of 'x + y' is: {x + y}");
+    }
+
+    [TestCase(5,6)]
+    public static void AddTwoIntegers( int number1 , int number2 )
+    {
+        var result = number1 + number2;
+        Console.WriteLine($"Adding '{number1}' and '{number2}' results in: {result}");
+    }
+}
