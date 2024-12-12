48e35dfd6a770b7db41006e9689c5cfb4eee6f39
ENDAVA\RFifiita <raduconstantin.fifiita@endava.com>
2024-12-12 14:10:08 +0200

    Session 8 - Conditional statements (if, if-else, if-else-if, switch)

---
+++ ConditionalStatements.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+
+namespace ConsoleAppETA25;
+public static class ConditionalStatements
+{
+    [Test]
+    public static void IfStatementTest()
+    {
+        int x = 20;
+        int y = 15;
+
+        if (x > y)
+        {
+            Console.WriteLine("x is greater than y");
+        }
+        if (x < y)
+        {
+            Console.WriteLine("x is smaller than y");
+        } 
+        else
+        {
+            Console.WriteLine("x is equal to y");
+        }
+
+        Console.WriteLine("Last test output line.");
+    }
+
+    [Test]
+    public static void SwitchStatementTest()
+    {
+        int dayOfWeek = 9;
+
+        switch(dayOfWeek)
+        {
+            case 2:
+                Console.WriteLine("Today is Tuesday");
+                break;
+            case 3:
+                Console.WriteLine("Today is Wednesday");
+                break;
+            case 4:
+                Console.WriteLine("Today is Thursday");
+                break;
+            case 5:
+                Console.WriteLine("Today is Friday");
+                break;
+            case 6:
+                Console.WriteLine("Today is Saturday");
+                break;
+            case 7:
+                Console.WriteLine("Today is Sunday");
+                break;
+            default:
+                Console.WriteLine("Invalid day number. A week only has 7 days!");
+                break;
+        }
+
+        Console.WriteLine("Last output line of the code");
+    }
+
+}
