abe6e39b5ef783a5ffe8fa8ef45f5147fad3a7c3
ENDAVA\RFifiita <raduconstantin.fifiita@endava.com>
2024-12-19 14:43:54 +0200

    subject, select dropdown, calendar menu + refactoring tests

---
+++ Automation/Session3/PracticeFormTests.cs
@@ -0,0 +1,208 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppETA25.Automation.Session3;
+public class PracticeFormTests
+{
+    public IWebDriver Driver;
+    public const string BaseUrl = "https://demoqa.com/";
+
+    public const string FirstName = "Radu";
+    public const string LastName = "Fifiita";
+    public const string Email = "rf-test@email.com";
+    public const string Age = "32";
+    public const string Salary = "50000";
+    public const string Department = "Software Dev";
+
+    [SetUp]
+    public void Setup()
+    {
+        // Initializare driver Chrome & pornire instanta browser
+        Driver = new ChromeDriver();
+
+        // Navigam la pagina de HOME: https://demoqa.com/
+        Driver.Navigate().GoToUrl(BaseUrl);
+
+        // Maximizare instanta browser
+        Driver.Manage().Window.Maximize();
+    }
+
+    [TestCase("Male")]
+    [TestCase("Female")]
+    [TestCase("Other")]
+    [TestCase("Monkey")]
+    public void GenderTest(string userInputGender)
+    {
+        // Define jsExecutor
+        IJavaScriptExecutor jsExecutor = (IJavaScriptExecutor)Driver;
+
+        // Scroll
+        jsExecutor.ExecuteScript("window.scrollTo(0, 500);");
+
+        // Definim si initializam un selector (pentru a identifica unic un nod in DOM-ul paginii)
+        By formsSelector = By.XPath("//h5[text()='Forms']");
+
+        // Initializam un webElement care ne permite interactiunea cu elementul din pagina
+        IWebElement formsButton = Driver.FindElement(formsSelector);
+
+        // Dam click pe element
+        formsButton.Click();
+
+        // Definim si initializam selector-ul pentru "Practice Form" side menu option
+        By practiceFormSelector = By.XPath("//span[text()='Practice Form']");
+
+        // Initialiam webElement-ul pentru Practice Form
+        IWebElement practiceFormOption = Driver.FindElement(practiceFormSelector);
+
+        // Dam click pe Practice Form option
+        practiceFormOption.Click();
+
+        // Definim si initializam selector-ul pentru Gender MALE
+        By genderMaleSelector = By.Id("gender-radio-1");
+
+        // Initializam webElement-ul pentru Gender MALE
+        IWebElement genderMaleInput = Driver.FindElement(genderMaleSelector);
+
+        // Definim si initializam selector-ul pentru Gender FEMALE
+        By genderFemaleSelector = By.Id("gender-radio-2");
+
+        // Initializam webElement-ul pentru Gender FEMALE
+        IWebElement genderFemaleInput = Driver.FindElement(genderFemaleSelector);
+
+        // Definim si initializam selector-ul pentru Gender OTHER
+        By genderOtherSelector = By.Id("gender-radio-3");
+
+        // Initializam webElement-ul pentru Gender OTHER
+        IWebElement genderOtherInput = Driver.FindElement(genderOtherSelector);
+
+        //if (genderMaleInput.GetDomAttribute("value") == userInputGender)
+        //{
+        //    jsExecutor.ExecuteScript("arguments[0].click();", genderMaleInput);
+        //}
+        //else if (genderFemaleInput.GetDomAttribute("value") == userInputGender)
+        //{
+        //    jsExecutor.ExecuteScript("arguments[0].click();", genderFemaleInput);
+        //}
+        //else
+        //{
+        //    jsExecutor.ExecuteScript("arguments[0].click();", genderOtherInput);
+        //}
+
+
+        // IF & SWITCH STATEMENT
+        By genderMaleLabelSelector = By.XPath("//label[@for=\"gender-radio-1\"]");
+        IWebElement genderMaleLabel = Driver.FindElement(genderMaleLabelSelector);
+
+        By genderFemaleLabelSelector = By.XPath("//label[@for=\"gender-radio-2\"]");
+        IWebElement genderFemaleLabel = Driver.FindElement(genderFemaleLabelSelector);
+
+        By genderOtherLabelSelector = By.XPath("//label[@for=\"gender-radio-3\"]");
+        IWebElement genderOtherLabel = Driver.FindElement(genderOtherLabelSelector);
+
+        //if (genderMaleLabel.Text == userInputGender)
+        //{
+        //    genderMaleLabel.Click();
+        //}
+        //else if (genderFemaleLabel.Text == userInputGender)
+        //{
+        //    genderFemaleLabel.Click();
+        //}
+        //else
+        //{
+        //    genderOtherLabel.Click();
+        //}
+
+        switch (userInputGender)
+        {
+            case "Male":
+                genderMaleLabel.Click(); 
+                break;
+            case "Female":
+                genderFemaleLabel.Click(); 
+                break;
+            default: 
+                genderOtherLabel.Click(); 
+                break;
+        }
+
+
+        //Sleep 5 sec
+        Thread.Sleep(5000);
+    }
+
+    [TestCase("Sport")]
+    [TestCase("Sports")]
+    [TestCase("Reading")]
+    [TestCase("Books")]
+    [TestCase("Music")]
+    public void HobbiesTest(string userInputHobby)
+    {
+        // Define jsExecutor
+        IJavaScriptExecutor jsExecutor = (IJavaScriptExecutor)Driver;
+
+        // Scroll
+        jsExecutor.ExecuteScript("window.scrollTo(0, 500);");
+
+        // Definim si initializam un selector (pentru a identifica unic un nod in DOM-ul paginii)
+        By formsSelector = By.XPath("//h5[text()='Forms']");
+
+        // Initializam un webElement care ne permite interactiunea cu elementul din pagina
+        IWebElement formsButton = Driver.FindElement(formsSelector);
+
+        // Dam click pe element
+        formsButton.Click();
+
+        // Definim si initializam selector-ul pentru "Practice Form" side menu option
+        By practiceFormSelector = By.XPath("//span[text()='Practice Form']");
+
+        // Initialiam webElement-ul pentru Practice Form
+        IWebElement practiceFormOption = Driver.FindElement(practiceFormSelector);
+
+        // Dam click pe Practice Form option
+        practiceFormOption.Click();
+
+        By hobbySportsSelector = By.XPath("//label[@for=\"hobbies-checkbox-1\"]");
+        IWebElement hobbySportsLabel = Driver.FindElement(hobbySportsSelector);
+
+        By hobbyReadingSelector = By.XPath("//label[@for=\"hobbies-checkbox-2\"]");
+        IWebElement hobbyReadingLabel = Driver.FindElement(hobbyReadingSelector);
+
+        By hobbyMusicSelector = By.XPath("//label[@for=\"hobbies-checkbox-3\"]");
+        IWebElement hobbyMusicLabel = Driver.FindElement(hobbyMusicSelector);
+
+        switch (userInputHobby)
+        {
+            case "Sport":
+            case "Sports":
+                hobbySportsLabel.Click(); 
+                break;
+            case "Reading":
+            case "Books":
+                hobbyReadingLabel.Click(); 
+                break;
+            case "Music":
+                hobbyMusicLabel.Click(); 
+                break;
+            default:
+                break;
+        }
+
+        Thread.Sleep(5000);
+    }
+
+    [TearDown]
+    public void CleanUp()
+    {
+        if (Driver != null)
+        {
+            Driver.Quit();
+            Driver.Dispose();
+        }
+    }
+}
---
+++ Automation/Session4/PracticeFormTestsV2.cs
@@ -0,0 +1,167 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace ConsoleAppETA25.Automation.Session4;
+
+public class PracticeFormTestsV2
+{
+    public IWebDriver Driver;
+    public const string BaseUrl = "https://demoqa.com/";
+
+    // Define jsExecutor
+    IJavaScriptExecutor jsExecutor;
+
+    [SetUp]
+    public void Setup()
+    {
+        // Initializare driver Chrome & pornire instanta browser
+        Driver = new ChromeDriver();
+
+        // Navigam la pagina de HOME: https://demoqa.com/
+        Driver.Navigate().GoToUrl(BaseUrl);
+
+        // Maximizare instanta browser
+        Driver.Manage().Window.Maximize();
+
+        // Initializare jsExecutor
+        jsExecutor = (IJavaScriptExecutor)Driver;
+
+        // Scroll
+        jsExecutor.ExecuteScript("window.scrollTo(0, 500);");
+    }
+
+    [TestCase("Accounting", "Maths")]
+    [TestCase("Maths", "Computer Science")]
+    [TestCase("English", "Biology")]
+    public void SubjectTest(string userInputSubject1, string userInputSubject2)
+    {
+        AccessPracticeForm();
+
+        // Scroll
+        jsExecutor.ExecuteScript("window.scrollTo(0, 200);");
+
+        // Initializam webElement-ul pentru Subjects input
+        IWebElement subjectsInput = Driver.FindElement(By.Id("subjectsInput"));
+
+        //Trimitem si selectam pe baza criteriilor in camp  
+        subjectsInput.SendKeys(userInputSubject1);
+        subjectsInput.SendKeys(Keys.Enter);
+
+        subjectsInput.SendKeys(userInputSubject2);
+        subjectsInput.SendKeys(Keys.Enter);
+
+        //Identificare buton de delete subiect din input field
+        //IWebElement subjectDeleteButtonV1 = Driver.FindElement(By.XPath($"//div[contains(@class,\"multiValue\")][./div[text()=\"{userInputSubject1}\"]]/div[2]"));
+        //IWebElement subjectDeleteButtonV2 = Driver.FindElement(By.XPath("//div[contains(@class,\"multiValue\")][./div[text()=\"English\"]]/div[contains(@class, \"__remove\")]"));
+        //IWebElement subjectDeleteButtonV3 = Driver.FindElement(By.XPath("//div[text()=\"English\"]/following-sibling::div"));
+        //IWebElement subjectDeleteButtonV4 = Driver.FindElement(By.XPath("//div[text()=\"English\"]/parent::div/div[2]"));
+
+        //IWebElement subjectDeleteButtonV5 = Driver.FindElement(By.XPath($"//div[contains(@class,\"multiValue\")][./div[text()=\"{userInputSubject2}\"]]/div[2]"));
+
+        RemoveSubject(userInputSubject1);
+
+        // Sleep
+        Thread.Sleep(5000);
+    }
+
+
+    [TestCase("NCR", "Delhi")]
+    public void StateAndCityTest(string userInputState, string userInputCity)
+    {
+        AccessPracticeForm();
+
+        // Identificam si initializam webElement-ul pentru State dropdown
+        IWebElement stateDropdown = Driver.FindElement(By.Id("react-select-3-input"));
+        
+        stateDropdown.SendKeys(userInputState);
+        stateDropdown.SendKeys(Keys.Enter);
+
+        // Identificam si initializam webElement-ul pentru City dropdown
+        IWebElement cityDropdown = Driver.FindElement(By.Id("react-select-4-input"));
+
+        cityDropdown.SendKeys(userInputCity);
+        cityDropdown.SendKeys(Keys.Enter);
+
+        // Sleep
+        Thread.Sleep(5000);
+    }
+
+    [TestCase("2023", "November", "29")]
+    public void DateOfBirthTest(string currentYear, string currentMonthName, string currentMonthDay)
+    {
+        AccessPracticeForm();
+
+        SelectDateFromCalendar(currentYear, currentMonthName, currentMonthDay);
+
+        Thread.Sleep(5000);
+    }
+
+    [TearDown]
+    public void CleanUp()
+    {
+        if (Driver != null)
+        {
+            Driver.Quit();
+            Driver.Dispose();
+        }
+    }
+
+    public void RemoveSubject(string subjectName)
+    {
+        var startingXpath = "//div[contains(@class,\"multiValue\")][./div[text()=\"";
+        var endingXpath = "\"]]/div[2]";
+        IWebElement subjectButton = Driver.FindElement(By.XPath($"{startingXpath}{subjectName}{endingXpath}"));
+
+        subjectButton.Click();
+    }
+
+    public void AccessPracticeForm()
+    {
+        // Definim si initializam un selector (pentru a identifica unic un nod in DOM-ul paginii)
+        By formsSelector = By.XPath("//h5[text()='Forms']");
+
+        // Initializam un webElement care ne permite interactiunea cu elementul din pagina
+        IWebElement formsButton = Driver.FindElement(formsSelector);
+
+        // Dam click pe element
+        formsButton.Click();
+
+        // Definim si initializam selector-ul pentru "Practice Form" side menu option
+        By practiceFormSelector = By.XPath("//span[text()='Practice Form']");
+
+        // Initialiam webElement-ul pentru Practice Form
+        IWebElement practiceFormOption = Driver.FindElement(practiceFormSelector);
+
+        // Dam click pe Practice Form option
+        practiceFormOption.Click();
+    }
+
+    public void SelectDateFromCalendar(string currentYear, string currentMonthName, string currentMonthDay)
+    {
+        // Identificam si initializam dateOfBirth input
+        IWebElement dateOfBirthInput = Driver.FindElement(By.Id("dateOfBirthInput"));
+
+        // Deschidem calendarul
+        dateOfBirthInput.Click();
+
+        // Initializam un Select element pentru year dropown
+        IWebElement yearDropdownWe = Driver.FindElement(By.XPath("//select[contains(@class, \"year-select\")]"));
+        var yearDropdown = new SelectElement(yearDropdownWe);
+
+        // Selectam luna
+        yearDropdown.SelectByValue(currentYear);
+
+        // Initializam un Select element pentru month dropown
+        IWebElement monthDropdownWe = Driver.FindElement(By.XPath("//select[contains(@class, \"month-select\")]"));
+        var monthDropdown = new SelectElement(monthDropdownWe);
+
+        // Selectam luna
+        monthDropdown.SelectByText(currentMonthName); 
+
+        // Selectam ziua
+        IWebElement dayOfCurrentMonth = Driver.FindElement(By.XPath($"//div[text()=\"{currentMonthDay}\" and not(contains(@class, \"--outside-month\"))]"));
+        dayOfCurrentMonth.Click();
+    }
+}
